78ee855b9a888aa664e8733588f512ccd7d31806
paul <4318471pkj@gmail.com>
2023-08-10 17:19:57 +0800

    bbbbbbbbbbb

---
+++ nim-uikit-android-master/nim-uikit-android-master/app/src/main/java/com/netease/yunxin/app/im/login/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.netease.yunxin.app.im.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+import com.netease.yunxin.app.im.R;
+import com.netease.yunxin.kit.common.ui.activities.BaseActivity;
+
+public class LoginActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+}
---
+++ nim-uikit-android-master/nim-uikit-android-master/app/src/main/java/com/netease/yunxin/app/im/login/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.netease.yunxin.app.im.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+import com.netease.yunxin.app.im.R;
+
+public class RegisterActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+
+}
